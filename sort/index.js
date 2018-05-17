8ae336301775f8f39a35bc6c04abbdf5d4bd72a6
caizongding <caizongding@gmail.com>
2018-05-17 23:06:30 +0800

    add bubble sort

--- sort/index.js
+++ sort/index.js
@@ -12,3 +12,32 @@ function insertionSort(array) {
         array[beforeIndex + 1] = element
     }
 }
+function bubbleSort(array) {
+    let isSorting = true;
+    for (let index = 0; index < array.length && isSorting; index++) {
+        isSorting = false;
+        for (let j = array.length - 2; j >= index; j--) {
+           if(array[j]> array[j+1]) {
+               // 前一位比后一位大，交换位置
+               const temp = array[j];
+               array[j] = array[j+1];
+               array[j+1] = temp;
+               isSorting = true;
+           }
+        }
+    }
+}
+function bubbleSort1(array){
+    for (let index = 0; index < array.length; index++) {
+        for (let j = index + 1; j < array.length; j++) {
+            if(array[index] > array[j]){
+                const temp = array[index];
+                array[index] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array
+}
+
+console.log(bubbleSort1([9,8,7,6,5,4,3]))
